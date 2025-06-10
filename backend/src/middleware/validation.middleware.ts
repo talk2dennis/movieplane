01d04458a7e2aba5b5d0075a2467d194f38590c7
talk2dennis <adigwedennis@gmail.com>
2025-06-10 10:23:16 +0100

    implemented and fixed minor bugs for authentication route

--- backend/src/middleware/validation.middleware.ts
+++ backend/src/middleware/validation.middleware.ts
@@ -5,11 +5,12 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        const formattedErrors = errors.array().map(err => ({
+        const formattedErrors = errors.array().map(( err) => ({
             param: err.type,
             message: err.msg
         }));
         return res.status(400).json({ errors: formattedErrors });
     }
+
     next();
 };
